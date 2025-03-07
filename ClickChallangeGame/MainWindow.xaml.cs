0a9e3b0d7147429f23a718c6ceb9e337ca026e0e
KovacsBars <kovibarsi@gmail.com>
2025-03-07 12:30:18 +0100

    Add project files.

---
+++ ClickChallangeGame/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ClickChallangeGame;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private int time = 10;
+    private DispatcherTimer Timer;
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Timer = new DispatcherTimer();
+        Timer.Interval = new TimeSpan(0, 0, 1);
+        Timer.Tick += Timer_Tick;
+    }
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        time--;
+        visszaszamlalo.Text = time.ToString();
+        if (time == 0)
+        {
+            Timer.Stop();
+            MessageBox.Show("Lejárt az idő");
+            kattintoButton.Visibility = Visibility.Collapsed;
+            startbutton.Visibility = Visibility.Visible;
+        }
+    }
+    private void ClickCounterButton(object sender, RoutedEventArgs e)
+    {
+        int counter = int.Parse(pontszamlalo.Text);
+        counter++;
+        pontszamlalo.Text = counter.ToString();
+    }
+
+    private void ClickStartButton(object sender, RoutedEventArgs e)
+    {
+        time = 10;
+        Timer.Start();
+        pontszamlalo.Text = "0";
+        kattintoButton.Visibility = Visibility.Visible;
+        startbutton.Visibility = Visibility.Collapsed;
+    }
+}
